5da2d67bb12a4c3a59a0ec9eeddd3913cff17a35
nayoung <evelyn82ny@gmail.com>
2022-09-04 03:09:09 +0900

    feat: add @NoArgsConstructor
    - test 환경에서 사용하기 위함

--- src/main/java/nayoung/reservation_system/web/account/model/SignUpRequest.java
+++ src/main/java/nayoung/reservation_system/web/account/model/SignUpRequest.java
@@ -1,8 +1,12 @@
 package nayoung.reservation_system.web.account.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class SignUpRequest {
 
     private String username;
