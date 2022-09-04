86bcaeb8ce37543ff0de2f83a434004e94a25c11
nayoung <evelyn82ny@gmail.com>
2022-09-04 14:17:47 +0900

    refactor: change to inherit EntityNotFoundException

--- src/main/java/nayoung/reservation_system/exception/global/NotFoundException.java
+++ src/main/java/nayoung/reservation_system/exception/global/NotFoundException.java
@@ -3,10 +3,11 @@ package nayoung.reservation_system.exception.global;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import nayoung.reservation_system.exception.ExceptionCode;
+import javax.persistence.EntityNotFoundException;
 
 @Getter
 @RequiredArgsConstructor
-public class NotFoundException extends IllegalArgumentException {
+public class NotFoundException extends EntityNotFoundException {
 
     private final ExceptionCode exceptionCode;
 }
